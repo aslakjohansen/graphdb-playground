720741984aeedc441a6d237d12d3c3cbf2fe8be9
Aslak Johansen <aslakj@gmail.com>
2025-08-04 22:12:03 +0200

    Added a note to self

--- lib/query/match.ex
+++ lib/query/match.ex
@@ -1,3 +1,4 @@
 defmodule Query.Match do
   @type t() :: Query.MatchNode.t() | Query.MatchBidi.t()
+  # TODO: Another way of modeling this is to essentially have a list along the way of [node1, {:bidi, edge, node}, {:forward, edge, node}, {:backward, edge, node} ]
 end
