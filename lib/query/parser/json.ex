d1ddb9206a517eb4c96a9e0fe0c649ac11bba5a8
Aslak Johansen <aslakj@gmail.com>
2025-08-05 16:27:32 +0200

    Wrote main parser function

--- lib/query/parser/json.ex
+++ lib/query/parser/json.ex
@@ -1,7 +1,31 @@
 defmodule Query.Parser.JSON do
   @spec parse(binary()) :: Query.MatchQuery.t()
   def parse(input) do
-    {:ok, data} = Jason.decode(input)
+    input
+    |> Jason.decode()
+    |> parse_match_query()
+  end
+
+  # helpers
+
+  defp parse_match_query({:error, _} = data) do
     data
+  end
+
+  defp parse_match_query({:ok, %{"match" => match, "where" => where, "return" => return}}) do
+    {_, match_part} = match_response = parse_match(match)
+    {_, where_part} = where_response = parse_where(where)
+    {_, return_part} = return_response = parse_return(return)
+
+    errors =
+      [match_response, where_response, return_response]
+      |> Enum.filter(fn {status, result} -> status == :error end)
+      |> Enum.map(fn {_, message} -> message end)
+
+    if length(errors) == 0 do
+      {:ok, Query.MatchQuery.new(match_part, where_part, return_part)}
+    else
+      {:error, errors}
+    end
   end
 end
