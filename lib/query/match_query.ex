6f21195121c6b57e48877ba151abd80d4e6719d4
Aslak Johansen <aslakj@gmail.com>
2025-08-05 15:42:54 +0200

    Added new function for Query.MatchQuery

--- lib/query/match_query.ex
+++ lib/query/match_query.ex
@@ -1,13 +1,19 @@
 defmodule Query.MatchQuery do
   defstruct match: nil,
-            omatch: nil,
             where: nil,
-            return: nil
+            return: nil,
+            omatch: nil
 
   @type t() :: %Query.MatchQuery{
           match: Query.Match.t(),
-          omatch: Query.Match.t(),
           where: Query.Expr.t(),
-          return: list(binary() | Query.Alias.t())
+          return: list(binary() | Query.Alias.t()),
+          omatch: Query.Match.t()
         }
+
+  @spec new(Query.Match.t(), Query.Expr.t(), list(Query.Alias.t()), Query.Match.t() | nil) ::
+          __MODULE__.t()
+  def new(match, where, return, omatch \\ nil) do
+    %__MODULE__{match: match, where: where, return: return, omatch: omatch}
+  end
 end
