33c9050f58fe30f11aea61bc445cb3836000efc6
Aslak Johansen <aslakj@gmail.com>
2025-08-08 18:59:22 +0200

    Filled out a function to start up node+edge registries and create some nodes and edges

--- lib/graphdb.ex
+++ lib/graphdb.ex
@@ -1,13 +1,29 @@
 defmodule Graphdb do
   def hello do
     {:ok, node_registry_pid} = Registry.start_link(name: :link_node_registry, keys: :unique)
+    {:ok, edge_registry_pid} = Registry.start_link(name: :link_edge_registry, keys: :unique)
 
     node1_id = "node 1"
+    node2_id = "node 2"
     edge1_id = "edge 1"
 
     {:ok, node1_pid} =
       Link.Node.start_link(node1_id, %{"modality" => "temperature"}, %{}, %{
         "hasLocation" => [edge1_id]
       })
+
+    {:ok, edge1_pid} =
+      Link.Edge.start_link(edge1_id, %{}, node1_id, node2_id)
+
+    {:ok, node2_pid} =
+      Link.Node.start_link(node2_id, %{}, %{"hasLocation" => [edge1_id]}, %{})
+
+    {:ok,
+     %{
+       node_registry: node_registry_pid,
+       edge_registry: edge_registry_pid,
+       nodes: [node1_pid, node2_pid],
+       edges: [edge1_pid]
+     }}
   end
 end
